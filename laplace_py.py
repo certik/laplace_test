a2e9e3e371cba51174ffb956cd43772ef6694466
Ondrej Certik <ondrej@certik.cz>
2011-06-22 20:58:10 -0700

    Add Python solution

---
+++ laplace_py.py
@@ -0,0 +1,21 @@
+from numpy import zeros
+from scipy import weave
+
+dx = 0.1
+dy = 0.1
+dx2 = dx*dx
+dy2 = dy*dy
+
+def py_update(u):
+    nx, ny = u.shape
+    for i in xrange(1,nx-1):
+        for j in xrange(1, ny-1):
+            u[i,j] = ((u[i+1, j] + u[i-1, j]) * dy2 +
+                      (u[i, j+1] + u[i, j-1]) * dx2) / (2*(dx2+dy2))
+
+def calc(N, Niter=100, func=py_update, args=()):
+    u = zeros([N, N])
+    u[0] = 1
+    for i in range(Niter):
+        func(u,*args)
+    return u
